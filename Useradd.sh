a0f1e6a6745d6fe2e4dd9bcd8805eae6ed67529a
Sai Simha Reddy <109609468+Simha4820@users.noreply.github.com>
2024-05-22 11:34:16 +0530

    Update Useradd.sh

--- Useradd.sh
+++ Useradd.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+# Backup necessary configuration files
 cp /etc/pam.d/system-auth /etc/pam.d/system-auth.bkp
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bkp
 cp /etc/login.defs /etc/login.defs.bkp
+
+# Modify configuration files
 sed -i '/pam_listfile.so/s/^/##/' /etc/pam.d/system-auth
 sed -i '/AllowGroups/s/^/##/' /etc/ssh/sshd_config
 sed -i '/AllowUsers/s/^/##/' /etc/ssh/sshd_config
-sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS   0/1' /etc/login.defs
-mv /etc/raddb/server /etc/raddb/server.bkp
-#sed -i '/AllowGroups/s/$/ lnx.admin1 lnx.admin2 lnx.admin3 /' sshd_config
+sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS   0/' /etc/login.defs
+
+# Backup the raddb server file if it exists
+if [ -f /etc/raddb/server ]; then
+    mv /etc/raddb/server /etc/raddb/server.bkp
+fi
+
+# Restart the sshd service
 /bin/systemctl restart sshd
 
-/bin/tee /tmp/userlist <<EOF
+# Create a list of users
+cat <<EOF > /tmp/userlist
 lnxpladmin
 lnx.admin1
 lnx.admin2
@@ -23,20 +32,13 @@ lnx.ro2
 lnx.ro3
 EOF
 
-userfile=/tmp/userlist
+# Set the password for the users
+password='My53cretP@55123'
 
-username=$(cat /tmp/userlist | tr 'A-Z'  'a-z')
-
-password=My53cretP@55123
-
-for user in $username
-do
-
-       useradd $user -d /home/PINESERVERS/$user
-       echo $password | passwd --stdin $user
+# Convert usernames to lowercase and create users
+for user in $(cat /tmp/userlist | tr 'A-Z' 'a-z'); do
+    useradd $user -d /home/PINESERVERS/$user
+    echo "$user:$password" | chpasswd
 done
-
-#echo "$(wc -l /tmp/userlist) users have been created"
-#tail -n$(wc -l /tmp userlist) /etc/passwd
 
 exit
